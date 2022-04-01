72feae04d157adf7c7bbab38cb8c28c51f26d8fe
Clowlar <10552924+clowlar@user.noreply.gitee.com>
2022-04-01 10:22:15 +0800

    second commit

---
+++ src/com0/test01.java
@@ -0,0 +1,13 @@
+package com0;
+
+public class test01 {
+    public static void main(String[] args) {
+        Person person=new Person();
+        person.setName("蔡徐坤");
+        person.setAge(15);
+        person.setSex(1);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        System.out.println(person.getSex());
+    }
+}
